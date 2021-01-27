f47e2996ca7c194757dc7c8c8c0ab8cbfb61ee46
Prashant Sedhain <sedhain.prashant@heb.com>
2021-01-27 00:51:20 -0600

    Added search bar

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { EvilIcons } from "@expo/vector-icons";
+
+import React from "react";
+import { StyleSheet, Text, TextInput, SafeAreaView, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flexDirection: "row",
+        // borderWidth: 1,
+        margin: 15,
+        height: 50,
+        marginTop: 50,
+        backgroundColor: "gray",
+        borderRadius: 10,
+      }}
+    >
+      <EvilIcons
+        style={styles.iconStyle}
+        name="search"
+        size={45}
+        color="white"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search Restaurants"
+        placeholderTextColor="white"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  inputStyle: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 18,
+    textAlign: "left",
+    color: "white",
+  },
+
+  iconStyle: {
+    alignSelf: "center",
+    marginHorizontal: 10,
   },
 });
