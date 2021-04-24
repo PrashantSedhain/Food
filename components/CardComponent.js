edb36203e6338ca9a1af591688c0d37c77da254f
Prashant Sedhain <49375658+PrashantSedhain@users.noreply.github.com>
2021-04-23 23:33:45 -0500

    favorite icon added: still needs work

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,16 +1,14 @@
 import React from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-} from "react-native";
-// import { ScrollView } from "react-native-gesture-handler";
+import { EvilIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 const CardComponent = (props) => {
+  const [iconClass, setIconClass] = useState({
+    class: styles.favIconStyle,
+    name: "heart-o",
+  });
   let Image_Http_URL = {
     uri: props.imageURL,
   };
@@ -27,7 +25,15 @@ const CardComponent = (props) => {
       <Text style={{ fontWeight: "bold", marginTop: 3, color: "#c4c4c4" }}>
         Gongabu, Kathmandu
       </Text>
-      {/* <Text style={{ color: "black", margin: 3 }}>$1.49 Delivery Fee</Text> */}
+
+      <FontAwesome
+        onPress={() => {
+          setIconClass({ class: styles.favClickedStyle, name: "heart" });
+        }}
+        style={iconClass.class}
+        name={iconClass.name}
+        size={35}
+      />
     </TouchableOpacity>
   );
 };
@@ -37,12 +43,10 @@ const styles = StyleSheet.create({
     margin: 10,
     width: 300,
     height: 200,
-    // borderWidth: 1,
     borderColor: "black",
     backgroundColor: "#F2F2F2",
     shadowOffset: { width: 10, height: 10 },
     shadowColor: "gray",
-    // shadowOpacity: 0.5,
     elevation: 3,
     zIndex: 1,
     borderRadius: 10,
@@ -51,7 +55,18 @@ const styles = StyleSheet.create({
   imageStyle: {
     height: 120,
     width: 300,
-    // flex: 1,
+  },
+
+  favIconStyle: {
+    position: "absolute",
+    margin: 7,
+    color: "white",
+  },
+
+  favClickedStyle: {
+    position: "absolute",
+    margin: 7,
+    color: "#f4511e",
   },
 });
 
